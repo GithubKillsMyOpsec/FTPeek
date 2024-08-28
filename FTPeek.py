7dffa87cb953f9c2a71394c6903578262430f58b
SierraUniformSierra <38790532+GithubKillsMyOpsec@users.noreply.github.com>
2024-08-28 09:33:51 -0600

    Update FTPeek.py
    V0.0.3

--- FTPeek.py
+++ FTPeek.py
@@ -1,9 +1,9 @@
 from FTPDL import monitor_ftp,regenerate_downloaded_files_pickle
-from AsynFTPDL import main
+from AsynFTPDL import AsyncDownloader
 import time
 import asyncio
 
-version = "0.0.2"
+version = "0.0.3"
 def interactive_mode():
     print("     ____   __ __       _             ")
     print("    / __ \ / //_/___   (_)____   _____")
@@ -14,12 +14,16 @@ def interactive_mode():
     print("FTPeek, FTP Monitoring and cloning tool.")
     print("This tool is for research purposes only.")
     print("Version " + version)
+    debug = False
+    waittime = 30
     while True:
         print("\n=======================================")
         print("\nSelect a module to run:")
         print("1. Monitor FTP")
         print("2. Regenerate PKL File Tracker")
         print("3. Run Async FTP Monitor (Beta)")
+        print("4. Set Check Time Delay")
+        print("5. Set Debug Flag")
         choice = input("Enter your choice: ")
         if choice == "1":
             webpls = input("Enter Discord Webhook (If applicable)")
@@ -27,17 +31,38 @@ def interactive_mode():
                 print("Discord Webhook detected.")
                 while True:
                     monitor_ftp(webpls, True)
-                    time.sleep(30)
+                    time.sleep(waittime)
             else:
                 print("No webhook detected. Manual Updating...")
                 while True:
                     monitor_ftp()
-                    time.sleep(30)
+                    time.sleep(waittime)
         elif choice =="2":
             print("Regenerating PKL File, please wait...")
             regenerate_downloaded_files_pickle()
         elif choice =="3":
-            asyncio.run(main())
+            webpls = input("Enter Discord Webhook (If applicable)")
+            if webpls != '':
+                print("Discord Webhook detected.")
+                while True:
+                    runner = AsyncDownloader(debugflag=debug, webhook=webpls)
+                    asyncio.run(runner.main())
+                    time.sleep(waittime)
+            else:
+                print("No webhook detected. Manual Updating...")
+                while True:
+                    runner = AsyncDownloader(debugflag=debug, webhook=None)
+                    asyncio.run(runner.main())
+                    time.sleep(waittime)
+        elif choice =="4":
+            waittime = input("Enter The Sleep Time (In Seconds)")
+        elif choice =="5":
+            whatistheflag = input("Enter Flag State (T/F)")
+            if whatistheflag == "T":
+                debug = True
+            else:
+                debug = False
+
 
 
 
